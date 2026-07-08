1c27200d1a7859fc77cf6afead4d856b098abe86
Lorenzo <Lorenzo@192.168.1.57>
2026-07-08 15:46:48 +0200

    correzione DAO

--- src/Dao/JDBCPlaylistPubblicaDao.java
+++ src/Dao/JDBCPlaylistPubblicaDao.java
@@ -1,5 +1,14 @@
 package Dao;
 
-public class JDBCPlaylistPubblicaDao implements PlaylistPubblicaDao{
+import java.sql.Connection;
 
+import Control.ControllerLogin;
+
+public class JDBCPlaylistPubblicaDao implements PlaylistPubblicaDao{
+	Connection conn;
+	ControllerLogin cLogin;
+	public JDBCPlaylistPubblicaDao(Connection conn, ControllerLogin cLogin) {
+		this.conn = conn;
+		this.cLogin=cLogin;
+	}
 }
